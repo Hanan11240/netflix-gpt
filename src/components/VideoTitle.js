d4516c65d0b4af4ca60a603254a662c07e33a88f
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-07-02 22:28:04 +0530

    video backfrou

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,10 +1,10 @@
 const VideoTitle = ({title,overview})=>{
     return (
-        <div className="pt-36 px-12">
+        <div className="w-screen aspect-video pt-[20%] px-36 absolute text-white bg-gradient-to-r from-black">
             <h1 className="text-6xl font-bold">{title}</h1>
             <p className="py-6 text-lg w-1/4">{overview}</p>
-            <div className="">
-                <button className="bg-gray-500 text-white px-16 text-lg py-4 rounded-lg mr-4 bg-opacity-50">⏯️ Play</button>
+            <div>
+                <button className="bg-white font-bold hover:bg-opacity-80 text-black px-16 text-lg py-4 rounded-lg mr-4 ">⏯️ Play</button>
                 <button className="bg-gray-500 text-white px-16 text-lg py-4 rounded-lg bg-opacity-50">ℹ️ More Info</button>
             </div>
         </div>
